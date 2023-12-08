59eb208a5cfc108c3ca19eb3cb3f71aab8aec0f8
Mark <mark@MacBook-Pro-Mark.local>
2023-12-08 16:21:42 +0300

    reelease 1

--- sudoku.js
+++ sudoku.js
@@ -1,23 +1,35 @@
+const fs = require('fs')
+const path = require('path')
 function read() {
-  /**
-   * Прочесть файл puzzles.txt в кодировке 'utf-8' и вернуть эти данные из функции
-   */
+  const fileName = './puzzles.txt'
+  const pathToFile = path.join(__dirname, fileName)
+  const readFile = fs.readFileSync(pathToFile, 'utf-8').trim().split('\n')[0]
+  
+  return readFile
 }
+read()
 
+const firstGame = read()
 function solve() {
+  function solvingSudoku(){
+  }
+  return firstGame
   /**
    * Принимает игровое поле в том формате, в котором его вернули из функции read.
    * Возвращает игровое поле после попытки его решить.
    */
 }
-
+console.log(solve());
 function isSolved() {
+  if (firstGame) {
+    return false
+  }
   /**
    * Принимает игровое поле в том формате, в котором его вернули из функции solve.
    * Возвращает булевое значение — решено это игровое поле или нет.
    */
 }
-
+console.log(isSolved());
 function prettyBoard() {
   /**
    * Принимает игровое поле в том формате, в котором его вернули из функции solve.
